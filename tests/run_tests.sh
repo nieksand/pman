6ba4502f517b585feaab08142a902cdfdfb3a262
Niek Sanders <niek.sanders@gmail.com>
2026-01-21 09:59:36 +0100

    Fix bash script to be agnostic of caller's directory.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,1 +1,3 @@
-PYTHONPATH=$(dirname $0)/.. python3 -m unittest -v *.py
+#!/bin/bash
+SCRIPT_DIR="$(dirname "$0")"
+PYTHONPATH="$SCRIPT_DIR/.." python3 -m unittest discover -v -s "$SCRIPT_DIR" -p "*.py"
